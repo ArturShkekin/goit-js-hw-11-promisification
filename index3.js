bc9fcfa039ae1476bf3c1b08853428c6ec9ebade
Artur Shkekin <64356091+ArturShkekin@users.noreply.github.com>
2021-05-23 16:28:29 +0300

    Сделал задание 2

--- index3.js
+++ index3.js
@@ -21,8 +21,6 @@ const randomIntegerFromInterval = (min, max) => {
   }, delay);
 };*/
 
-
-
 const makeTransaction = (transaction) => {
     const delay = randomIntegerFromInterval(200, 500);
   return new Promise((resolve, reject) => {
@@ -30,7 +28,7 @@ const makeTransaction = (transaction) => {
         const canProcess = Math.random() > 0.3;
         
           if (canProcess) {
-              resolve(transaction.id, delay);
+              resolve([transaction.id, delay]);
           } else {
               reject (transaction.id)
         }
@@ -38,22 +36,13 @@ const makeTransaction = (transaction) => {
   });
 };
 
-const logSuccess = (id, time) => {
+const logSuccess = ([id, time]) => {
   console.log(`Transaction ${id} processed in ${time}ms`);
 };
 const logError = id => {
   console.warn(`Error processing transaction ${id}. Please try again later.`);
 };
 
-/*
- * Работает так
-makeTransaction({ id: 70, amount: 150 }, logSuccess, logError);
-makeTransaction({ id: 71, amount: 230 }, logSuccess, logError);
-makeTransaction({ id: 72, amount: 75 }, logSuccess, logError);
-makeTransaction({ id: 73, amount: 100 }, logSuccess, logError);
- */
-
-// Должно работать так
 makeTransaction({ id: 70, amount: 150 })
   .then(logSuccess)
   .catch(logError);
